d84ba30de00ae01f4eb9001c585a0fc41c8de703
“estevaomrviana” <estevaomagnoviana@gmail.com>
2018-04-10 12:25:00 -0300

    envio de tela Cadastrar

--- AlmondesSol/src/almondessol/AlmondesSol.java
+++ AlmondesSol/src/almondessol/AlmondesSol.java
@@ -16,6 +16,8 @@ public class AlmondesSol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        TelaCadastrar cadastrar = new TelaCadastrar();
+        cadastrar.setVisible(true);
     }
     
 }
