4a2ba36f38051c2da86cc588f6a9126ad8725a3d
“estevaomrviana” <estevaomagnoviana@gmail.com>
2018-04-11 01:55:10 -0300

    Alteracao em cadastro

--- AlmondesSol/test/almondessol/CadASTest.java
+++ AlmondesSol/test/almondessol/CadASTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.*;
 public class CadASTest {
     Instrumento i;
     CadAS cad;
+    TelaCadastrar tcad;
     @Before
     public void setUp() {
         i = new Instrumento("Violão", "Fender","CD-60CE",  "Instrumento de Corda","Violão Preto com abertura Lateral", 2.8, 3);
@@ -28,9 +29,10 @@ public class CadASTest {
     @Test
     public void cadastroCompletotest() {
         cad = new CadAS(i);
-        assertTrue("O texte deve fuincionar sem Falhas", cad.cadastro());
+        assertTrue("O texte deve funcionar sem Falhas", cad.cadastro());
     }
     
+/*    
     @Test
     public void faltodeCampoNometest() {
         i.setNome("");
@@ -64,27 +66,65 @@ public class CadASTest {
         i.setDescricao("");
         cad = new CadAS(i);
         assertFalse("O cadastro de Instrumentos deve ter Descricao do instrumento", cad.cadastro());
+    }*/
+    
+    public void stringVaziaTest() {
+        i.setNome("");
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Nome do instrumento", cad.cadastro());
+        
+        i.setMarca("");
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Marca do instrumento", cad.cadastro());
+        
+        i.setModelo("");
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Modelo do instrumento", cad.cadastro());
+        
+        i.setDescricao("");
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Descricao do instrumento", cad.cadastro());
+        
+        i.setTipo("");
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Tipo do instrumento", cad.cadastro());
+        
+        i.setPreco(Double.valueOf("2.6"));
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Preço do instrumento", cad.cadastro());
+        
+        i.setQuant(Integer.valueOf("1"));
+        cad = new CadAS(i);
+        assertFalse("O cadastro de Instrumentos deve ter Quantidade do instrumento", cad.cadastro());
     }
     
+    @Test
+    public void numeroEmCampoDeLetrasTest(){
+        i.setNome("e");
+        cad = new CadAS(i);
+        assertFalse("Não é permitido número neste tipo de campo",tcad.letrasEmCampoDeNumero(i.getNome()));
+    }
     
     @Test
-    public void precoNegativotest() {
+    public void precoNegativoTest() {
         i.setPreco(-1.8);
         cad = new CadAS(i);
         assertFalse("O preco não pode ser Negativo", cad.cadastro());
     }
     
     @Test
-    public void precoZerotest() {
+    public void precoZeroTest() {
         i.setPreco(0);
         cad = new CadAS(i);
         assertFalse("O preco não pode ser Negativo", cad.cadastro());
     }
     
     @Test
-    public void quantNegativotest() {
+    public void quantNegativoTest() {
         i.setPreco(-1.8);
         cad = new CadAS(i);
         assertFalse("O preco não pode ser Negativo", cad.cadastro());
     }
+    
+    
 }
